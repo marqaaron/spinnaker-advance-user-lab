f461cff7972c285a0da52e24b2523010c218102e
Mark Noe <mark.a.noe@gmail.com>
2020-12-26 09:40:44 -0800

    Refactor Authentication.vue module to remove unused components and configure for local development without API calls.

--- src/modules/authentication/Authentication.vue
+++ src/modules/authentication/Authentication.vue
@@ -4,26 +4,10 @@
       <b-col cols="8" offset="2">
         <b-row>
           <b-col>
-            <h4 class="text-center" v-if="form === 'forgot-username-form'">Request Account Reminder</h4>
-            <h4 class="text-center" v-else-if="form === 'forgot-password-form'">Request Password Reset</h4>
-            <h4 class="text-center" v-else>Welcome Back.</h4>
+            <h4 class="text-center">Welcome Back.</h4>
           </b-col>
         </b-row>
-        <login-form v-if="form === 'login-form'"></login-form>
-        <login-button v-else-if="form === 'login-button'"></login-button>
-        <forgot-password-form v-else-if="form === 'forgot-password-form'"></forgot-password-form>
-        <forgot-email-form v-else></forgot-email-form>
-        <b-row class="mt-2">
-          <b-col>
-            <div v-if="form === 'login-form'">
-              <b-button variant="outline-secondary" block @click="toggleForm('forgot-username-form')">Forgot Email</b-button>
-              <b-button variant="outline-secondary" block @click="toggleForm('forgot-password-form')">Forgot Password</b-button>
-            </div>
-            <div v-else-if="form !== 'login-form' && form !== 'login-button'">
-              <b-button variant="outline-secondary" block @click="toggleForm('login-form')">Back to Login</b-button>
-            </div>
-          </b-col>
-        </b-row>
+        <login-button></login-button>
       </b-col>
     </b-row>
   </div>
@@ -31,16 +15,12 @@
 
 <script>
   import {mapGetters} from "vuex";
-  import LoginForm from "@/modules/authentication/components/LoginForm";
-  import ForgotPasswordForm from "@/modules/authentication/components/ForgotPasswordForm";
-  import ForgotEmailForm from "@/modules/authentication/components/ForgotEmailForm";
   import LoginButton from "@/modules/authentication/components/LoginButton";
 
   export default {
     name: "Authentication",
     data() {
       return {
-        form: 'login-form',
         activeMenu: 'authentication',
         defaultContent: 'login'
       }
@@ -49,14 +29,9 @@
       let activeContent = this.defaultContent;
       this.$store.dispatch('setActiveMenu',this.activeMenu);
       this.$store.dispatch('setActiveContent',activeContent);
-      if(process.env.VUE_APP_AUTHENTICATION_TYPE === 'third_party_idp'){
-        this.form = 'login-button';
-      }
     },
     methods: {
-      toggleForm(_value) {
-        this.form = _value;
-      }
+
     },
     computed: {
       ...mapGetters([
@@ -65,9 +40,6 @@
       ])
     },
     components: {
-      'login-form': LoginForm,
-      'forgot-password-form': ForgotPasswordForm,
-      'forgot-email-form': ForgotEmailForm,
       'login-button': LoginButton
     },
     watch: {
--- src/modules/authentication/store/authentication.js
+++ src/modules/authentication/store/authentication.js
@@ -1,6 +1,7 @@
 import api from "@/core/utilities/api";
 import log from "@/core/utilities/log";
 import gateEndpoints from "@/modules/authentication/store/gateEndpoints";
+import {user} from "@/modules/authentication/store/mockData";
 
 const config = process.env;
 
@@ -33,141 +34,80 @@ export default {
             context.commit("setInvalidSessionAlertActive",payload);
         },
         logIn({commit,getters},payload){
-            return new Promise( (resolve,reject) => {
-                log.text("Requesting Login from " + config.VUE_APP_ENDPOINT_LOGIN);
-                api.post(
-                    config.VUE_APP_ENDPOINT_LOGIN,
-                    payload
-                ).then(
-                    (response) => {
-                        log.text("Login successful");
+            if(config.NODE_ENV !== 'development'){
+                window.open(gateEndpoints.loginRedirectUrl(process.env),'_self');
+            } else {
+                return new Promise( (resolve,reject) => {
+                    log.text("Logging in locally");
+                    setTimeout(()=>{
                         commit("toggleIsLoggedIn",true);
-                        commit("setUserDetails",response.data);
+                        commit("setUserDetails",user);
                         resolve(true);
-                    },
-                    (error) => {
-                        log.obj("Login Request Error",error);
-                        reject(api.error(error));
-                    }
-                );
-            })
+                    },1000);
+                })
+            }
         },
         requestUser({commit,getters},payload){
             return new Promise( (resolve,reject) => {
-                log.text("Requesting User from " + gateEndpoints.userDetailsUrl(config));
-                api.get(
-                    gateEndpoints.userDetailsUrl(config)
-                ).then(
-                    (response) => {
-                        log.text("User Details Request successful");
-                        if(typeof response.data.email !== 'undefined'){
-                            commit("toggleIsLoggedIn",true);
-                            commit("setUserDetails",response.data);
-                            resolve(true);
-                        } else {
-                            resolve(false);
+                if(config.NODE_ENV !== 'development'){
+                    log.text("Requesting User from " + gateEndpoints.userDetailsUrl(config));
+                    api.get(
+                        gateEndpoints.userDetailsUrl(config)
+                    ).then(
+                        (response) => {
+                            log.text("User Details Request successful");
+                            if(typeof response.data.email !== 'undefined'){
+                                commit("toggleIsLoggedIn",true);
+                                commit("setUserDetails",response.data);
+                                resolve(true);
+                            } else {
+                                resolve(false);
+                            }
+                        },
+                        (error) => {
+                            log.obj("Login Request Error",error);
+                            reject(api.error(error));
                         }
-                    },
-                    (error) => {
-                        log.obj("Login Request Error",error);
-                        reject(api.error(error));
-                    }
-                );
+                    );
+                } else {
+                    log.text("User Details Request skipped in Dev Environment");
+                    setTimeout(()=>{
+                        resolve(true);
+                    },1000);
+                }
+
             })
         },
         logout(context,payload){
             return new Promise((resolve,reject)=>{
-                log.text("Requesting Logout from " + gateEndpoints.logOutUrl(config));
-                api.get(
-                    gateEndpoints.logOutUrl(config)
-                ).then(
-                    (response) => {
-                        log.text("Logout Request successful");
+                if(config.NODE_ENV !== 'development'){
+                    log.text("Requesting Logout from " + gateEndpoints.logOutUrl(config));
+                    api.get(
+                        gateEndpoints.logOutUrl(config)
+                    ).then(
+                        (response) => {
+                            log.text("Logout Request successful");
+                            context.commit("setUserDetails",false);
+                            context.commit("toggleIsLoggedIn",false);
+                            resolve(true);
+                        },
+                        (error) => {
+                            log.obj("Login Request Error",error);
+                            reject(api.error(error));
+                        }
+                    );
+                } else {
+                    log.text("Logging out locally");
+                    setTimeout(()=>{
                         context.commit("setUserDetails",false);
                         context.commit("toggleIsLoggedIn",false);
                         resolve(true);
-                    },
-                    (error) => {
-                        log.obj("Login Request Error",error);
-                        reject(api.error(error));
-                    }
-                );
-            })
-        },
-        requestEmailReminder({commit,getters},payload){
-            return new Promise((resolve,reject) => {
-                log.text("Requesting Email Reminder from " + config.VUE_APP_ENDPOINT_FORGOT_EMAIL);
-                api.post(
-                    config.VUE_APP_ENDPOINT_FORGOT_EMAIL,
-                    payload
-                ).then(
-                    (response) => {
-                        log.text("Forgot Email request received successfully");
-                        resolve(true);
-                    },
-                    (error) => {
-                        log.obj("Forgot Email Request Error",error);
-                        reject(api.error(error));
-                    }
-                )
-            })
-        },
-        requestPasswordReset({commit,getters},payload){
-            return new Promise((resolve,reject) => {
-                log.text("Requesting Password Reset from " + config.VUE_APP_ENDPOINT_PASSWORD_RESET_REQUEST);
-                api.post(
-                    config.VUE_APP_ENDPOINT_PASSWORD_RESET_REQUEST,
-                    payload
-                ).then(
-                    (response) => {
-                        log.text("Password Reset request received successfully");
-                        resolve(true);
-                    },
-                    (error) => {
-                        log.obj("Password Reset Request Error",error);
-                        reject(api.error(error));
-                    }
-                )
-            })
-        },
-        validatePasswordResetToken({commit,getters},payload){
-            return new Promise((resolve,reject) => {
-                log.text("Validating Password Reset Token from " + config.VUE_APP_ENDPOINT_VALIDATE_PASSWORD_RESET_TOKEN);
-                api.post(
-                    config.VUE_APP_ENDPOINT_VALIDATE_PASSWORD_RESET_TOKEN,
-                    payload
-                ).then(
-                    (response) => {
-                        log.text("Password Reset Token Validation request received successfully");
-                        resolve(true);
-                    },
-                    (error) => {
-                        log.obj("Password Reset Token Validation Request Error",error);
-                        reject(api.error(error));
-                    }
-                )
+                    },1000);
+                }
             })
         },
         setAuthenticationEnabled(context,payload){
             context.commit('setAuthenticationEnabled',payload);
-        },
-        resetPassword({commit,getters},payload) {
-            return new Promise((resolve, reject) => {
-                log.text("Resetting Password from " + config.VUE_APP_ENDPOINT_RESET_PASSWORD);
-                api.post(
-                    config.VUE_APP_ENDPOINT_RESET_PASSWORD,
-                    payload
-                ).then(
-                    (response) => {
-                        log.text("Password Reset received successfully");
-                        resolve(true);
-                    },
-                    (error) => {
-                        log.obj("Password Reset Request Error", error);
-                        reject(api.error(error));
-                    }
-                )
-            })
         }
     },
     mutations: {
