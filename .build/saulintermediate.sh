6ce01e8413ddc37d06285df4d4f01ff01fbc1bd9
Mark Noe <mark.a.noe@gmail.com>
2021-01-09 08:27:13 -0800

    (fix) Minor fixes to saulintermediate.sh build script to include removing /dist folder in the event it exists which speeds up this phase of build.

--- .build/saulintermediate.sh
+++ .build/saulintermediate.sh
@@ -5,15 +5,17 @@ echo "--------- Saul Intermediate Logs -----------"
 echo "Installing required packages"
 apk add --no-cache git nodejs npm
 
-echo "Moving into /app directory"
+echo "Moving into /ui/app directory"
 cd /ui/app
 
-echo "Removing existing node_modules if it exists"
-
+echo "Removing existing ./node_modules directory if it exists"
 rm -rf node_modules
 
 echo "Installing npm modules"
 npm ci
+
+echo "Removing existing ./dist directory if it exists"
+rm -rf dist
 
 echo "Building Production App"
 npm run build
